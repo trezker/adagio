d5df427f9f2dc74c6d9433ca3a86840caccf4d83
Anders Andersson <trezker@gmail.com>
2009-08-06 12:09:59 +0200

    Example now also runs without  errors

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -6,9 +6,6 @@
 
 namespace adagio
 {
-	void Init_default_widgets(Widget_factory &wf, Resource_manager &rm)
-	{
-	}
 	class Group: public Widget
 	{
 	public:
@@ -24,87 +21,102 @@ namespace adagio
 		{
 		}
 	};
+
+	void Init_default_widgets(Widget_factory &wf, Resource_manager &rm)
+	{
+		Button* button = new Button;
+		wf.Set_prototype("button", button);
+		Group* group = new Group;
+		wf.Set_prototype("group", group);
+	}
+	
 }
 
 int main()
 {
-	al_init();
-	al_install_mouse();
-	al_install_keyboard();
-	al_init_iio_addon();
-	al_init_font_addon();
-	al_init_ttf_addon();
-	
-	ALLEGRO_DISPLAY *display;
-	al_set_new_display_flags(ALLEGRO_WINDOWED);
-	display = al_create_display(640, 480);
+	try{
+		al_init();
+		al_install_mouse();
+		al_install_keyboard();
+		al_init_iio_addon();
+		al_init_font_addon();
+		al_init_ttf_addon();
+		
+		ALLEGRO_DISPLAY *display;
+		al_set_new_display_flags(ALLEGRO_WINDOWED);
+		display = al_create_display(640, 480);
 
-	ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
-	al_register_event_source(event_queue, (ALLEGRO_EVENT_SOURCE *)display);
-	al_register_event_source(event_queue, (ALLEGRO_EVENT_SOURCE *)al_get_keyboard());
-	al_register_event_source(event_queue, (ALLEGRO_EVENT_SOURCE *)al_get_mouse());
+		ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
+		al_register_event_source(event_queue, (ALLEGRO_EVENT_SOURCE *)display);
+		al_register_event_source(event_queue, (ALLEGRO_EVENT_SOURCE *)al_get_keyboard());
+		al_register_event_source(event_queue, (ALLEGRO_EVENT_SOURCE *)al_get_mouse());
 
-	adagio::Resource_manager resource_manager;
-	adagio::Widget_factory widget_factory;
-	adagio::Init_default_widgets(widget_factory, resource_manager); //Sets up default widgets
-	//Optional, modify the widget prototypes, do skinning and behaviour changes, add custom widgets.
+		adagio::Resource_manager resource_manager;
+		adagio::Widget_factory widget_factory;
+		adagio::Init_default_widgets(widget_factory, resource_manager); //Sets up default widgets
+		//Optional, modify the widget prototypes, do skinning and behaviour changes, add custom widgets.
 
-	//Create and set up widgets
-	adagio::Button* button = widget_factory.Clone<adagio::Button>("button");
-	button->Set_label("Ze button");
+		//Create and set up widgets
+		adagio::Button* button = widget_factory.Clone<adagio::Button>("button");
+		button->Set_label("Ze button");
 
-	adagio::Group* root = widget_factory.Clone<adagio::Group>("group");
-	root->Add_widget(button);
+		adagio::Group* root = widget_factory.Clone<adagio::Group>("group");
+		root->Add_widget(button);
 
-	adagio::Event_queue gui_event_queue;
-	root->Set_event_queue(&gui_event_queue); //All widgets send events to parent.
+		adagio::Event_queue gui_event_queue;
+		root->Set_event_queue(&gui_event_queue); //All widgets send events to parent.
 
-//	double last_time = al_current_time();
-	bool quit = false;
-	while(!quit)
-	{
-		ALLEGRO_EVENT event;
-		if (al_get_next_event(event_queue, &event))
+	//	double last_time = al_current_time();
+		bool quit = false;
+		while(!quit)
 		{
-		  	if (ALLEGRO_EVENT_KEY_DOWN == event.type)
+			ALLEGRO_EVENT event;
+			if (al_get_next_event(event_queue, &event))
 			{
-				if (ALLEGRO_KEY_ESCAPE == event.keyboard.keycode)
+				if (ALLEGRO_EVENT_KEY_DOWN == event.type)
+				{
+					if (ALLEGRO_KEY_ESCAPE == event.keyboard.keycode)
+					{
+						break;
+					}
+				}
+				if (ALLEGRO_EVENT_DISPLAY_CLOSE == event.type)
 				{
 					break;
 				}
+				root->Handle_event(event);
 			}
-			if (ALLEGRO_EVENT_DISPLAY_CLOSE == event.type)
+
+	/*		double current_time = al_current_time();
+			double dt = current_time - last_time;
+			last_time = current_time;
+	*/
+			adagio::Event gui_event;
+			while(gui_event_queue.Get_next_event(gui_event))
 			{
-				break;
+				if(button == gui_event.source && adagio::Button::EVENT_PRESSED == gui_event.type)
+				{
+					//do stuff
+				}
 			}
-			root->Handle_event(event);
+			//Update other stuff
+
+			//Render other stuff
+			root->Render();
+			al_flip_display();
+			al_clear_to_color(al_map_rgb(0, 0, 0));
+			al_rest(0.001);
 		}
 
-/*		double current_time = al_current_time();
-		double dt = current_time - last_time;
-		last_time = current_time;
-*/
-		adagio::Event gui_event;
-		while(gui_event_queue.Get_next_event(gui_event))
-		{
-			if(button == gui_event.source && adagio::Button::EVENT_PRESSED == gui_event.type)
-			{
-				//do stuff
-			}
-		}
-		//Update other stuff
+		delete button;
+		delete root;
 
-		//Render other stuff
-		root->Render();
-		al_flip_display();
-		al_clear_to_color(al_map_rgb(0, 0, 0));
-		al_rest(0.001);
+		al_destroy_event_queue(event_queue);
+		al_destroy_display(display);
 	}
-
-	delete button;
-	delete root;
-
-	al_destroy_event_queue(event_queue);
-	al_destroy_display(display);
+	catch(const char* param)
+	{
+		printf("%s\n", param);
+	}
 	return 0;
 }
